38c68c66c7ef9b062970145e43b21af3eddb2256
Sander Backus <info@sanbax.com>
2018-04-18 13:52:58 +0200

    added domready dependency + package.lock

--- src/landingpage.js
+++ src/landingpage.js
@@ -1,34 +1,38 @@
 (function() {
-	var BrowserStorage = require('./browserstorage')();
+	var domready = require("domready");
 
-	document.currentScript = document.currentScript || (function() {
-		var scripts = document.querySelectorAll('[data-name="oalanding"]');
-		if (scripts[0]) {
-			return scripts[0];
-		}
-		scripts = document.querySelectorAll('[data-advertiser]');
-		return scripts[0];
-	})();
-
-	if (document.currentScript) {
-		var param = document.currentScript.getAttribute('data-param') || document.currentScript.getAttribute('data-params') || 'oa_id';
-		var params = param.split(',');
-		var cookie_days = document.currentScript.getAttribute('data-cookie-days') || '30';
-
-		var click_id = false; 
-		for (var i=0;i<params.length;i++) {
-			var x = params[i].replace(/^\s+|\s+$/gm,'');
-			if (x.length) {
-				click_id = decodeURIComponent((new RegExp('[?|&]' + x + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+	domready(function(){
+		var BrowserStorage = require('./browserstorage')();
 	
-				if(click_id) {
-					break;
+		document.currentScript = document.currentScript || (function() {
+			var scripts = document.querySelectorAll('[data-name="oalanding"]');
+			if (scripts[0]) {
+				return scripts[0];
+			}
+			scripts = document.querySelectorAll('[data-advertiser]');
+			return scripts[0];
+		})();
+	
+		if (document.currentScript) {
+			var param = document.currentScript.getAttribute('data-param') || document.currentScript.getAttribute('data-params') || 'oa_id';
+			var params = param.split(',');
+			var cookie_days = document.currentScript.getAttribute('data-cookie-days') || '30';
+	
+			var click_id = false; 
+			for (var i=0;i<params.length;i++) {
+				var x = params[i].replace(/^\s+|\s+$/gm,'');
+				if (x.length) {
+					click_id = decodeURIComponent((new RegExp('[?|&]' + x + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+		
+					if(click_id) {
+						break;
+					}
 				}
 			}
+	
+			if (click_id) {
+				BrowserStorage.set('oa-click-id', click_id, cookie_days);
+			}
 		}
-
-		if (click_id) {
-			BrowserStorage.set('oa-click-id', click_id, cookie_days);
-		}
-	}
+	});
 }).call(this);
--- src/prelander.js
+++ src/prelander.js
@@ -1,61 +1,127 @@
 (function() {
-	document.currentScript = document.currentScript || (function() {
-		var scripts = document.querySelectorAll('[data-params]');
-		if (scripts[0]) {
-			return scripts[0];
-		}
-	})();
+	var domready = require("domready");
 
-	var params = ['oa_clickid'];
-	var link_selector = 'a[href], area[href]';
-	var form_selector = 'form';
-	var iframe_selector = 'iframe'; 
-
-	if (document.currentScript) {
-		var paramsdata = document.currentScript.getAttribute('data-params');
-		var params = paramsdata.split(',');
-		var linkselector_data = document.currentScript.getAttribute('data-link-selector');
-		var formselector_data = document.currentScript.getAttribute('data-form-selector');
-		var iframeselector_data = document.currentScript.getAttribute('data-iframe-selector');
-
-		if(linkselector_data) {
-			link_selector = linkselector_data;
-		}
-		if(formselector_data) {
-			form_selector = formselector_data;
-		}
-		if(iframeselector_data ) {
-			iframe_selector = iframeselector_data;
-		}
-	}
-
-	var found_params = [];
-	var query_string_parts = [];
-
-	for (var i=0;i<params.length;i++) {
-		var x = params[i].replace(/^\s+|\s+$/gm,'');
-		if (x.length) {
-			var value = decodeURIComponent((new RegExp('[?|&]' + x + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
-
-			if(value) {
-				found_params.push({
-					name: x,
-					value: value
-				});
-
-				query_string_parts.push(encodeURIComponent( x ) + "=" + encodeURIComponent( value ))
+	domready(function(){
+		document.currentScript = document.currentScript || (function() {
+			var scripts = document.querySelectorAll('[data-params]');
+			if (scripts[0]) {
+				return scripts[0];
+			}
+		})();
+	
+		var params = ['oa_clickid'];
+		var link_selector = 'a[href], area[href]';
+		var form_selector = 'form';
+		var iframe_selector = 'iframe'; 
+	
+		if (document.currentScript) {
+			var paramsdata = document.currentScript.getAttribute('data-params');
+			var params = paramsdata.split(',');
+			var linkselector_data = document.currentScript.getAttribute('data-link-selector');
+			var formselector_data = document.currentScript.getAttribute('data-form-selector');
+			var iframeselector_data = document.currentScript.getAttribute('data-iframe-selector');
+	
+			if(linkselector_data) {
+				link_selector = linkselector_data;
+			}
+			if(formselector_data) {
+				form_selector = formselector_data;
+			}
+			if(iframeselector_data ) {
+				iframe_selector = iframeselector_data;
 			}
 		}
-	}
-
-	if(found_params.length) {
-		// build query string
-		var query_string = query_string_parts.join("&");
-
-		// add to a-links
-		var links = document.querySelectorAll(link_selector);
-		for(var i=0;i<links.length;i++) {
-			var link = links[i].getAttribute('href');
+	
+		var found_params = [];
+		var query_string_parts = [];
+	
+		for (var i=0;i<params.length;i++) {
+			var x = params[i].replace(/^\s+|\s+$/gm,'');
+			if (x.length) {
+				var value = decodeURIComponent((new RegExp('[?|&]' + x + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+	
+				if(value) {
+					found_params.push({
+						name: x,
+						value: value
+					});
+	
+					query_string_parts.push(encodeURIComponent( x ) + "=" + encodeURIComponent( value ))
+				}
+			}
+		}
+	
+		if(found_params.length) {
+			// build query string
+			var query_string = query_string_parts.join("&");
+	
+			// add to a-links
+			var links = document.querySelectorAll(link_selector);
+			for(var i=0;i<links.length;i++) {
+				var link = links[i].getAttribute('href');
+				if(link.length && link.charAt(0) != '#') {
+					var anchor_pos = link.indexOf('#');
+					var anchorcontent = '';
+					if(anchor_pos > -1) {
+						anchorcontent = link.substring(anchor_pos+1);
+						link = link.substring(0, anchor_pos);
+					}
+	
+					link += (link.indexOf('?') > -1 ? '&' : '?') + query_string;
+					if(anchorcontent.length) {
+						link += '#' + anchorcontent;
+					}
+	
+					links[i].setAttribute('href', link);
+				}
+			}
+	
+			// add to forms
+			var forms = document.querySelectorAll(form_selector);
+			for (var i=0;i<forms.length;i++) {
+				var action = forms[i].getAttribute('action');
+				var method = forms[i].getAttribute('method');
+	
+				// if post, add to action, otherwise add hidden inputs.
+				if (method && method.toLowerCase() == 'post') {
+					action = action === null ? '' : action; ;
+					if (action.charAt(0) != '#') {
+						var anchor_pos = action.indexOf('#');
+						var anchorcontent = '';
+						if (anchor_pos > -1) {
+							anchorcontent = action.substring(anchor_pos+1);
+							action = action.substring(0, anchor_pos);
+						}
+	
+						action += (link.indexOf('?') > -1 ? '&' : '?') + query_string;
+						if (anchorcontent.length) {
+							action += '#' + anchorcontent;
+						}
+	
+						forms[i].setAttribute('action', action);
+					}
+				} else {
+					for (var j=0;j<found_params.length;j++) {
+						var p = found_params[j];
+						var input = document.createElement('input');
+						input.setAttribute('name', p.name);
+						input.setAttribute('value', p.value);
+						input.setAttribute('type', 'hidden');
+						forms[i].appendChild(input);
+					}
+				}
+			}
+		} else {
+			var query_string = '';
+		}
+		
+		// add to iframes
+		var iframes = document.querySelectorAll(iframe_selector);
+		for(var i=0;i<iframes.length;i++) {
+			var link = iframes[i].getAttribute('src');
+			if (!link || link == undefined || link.length == 0) {
+				link = iframes[i].getAttribute('data-original-src');
+			}
 			if(link.length && link.charAt(0) != '#') {
 				var anchor_pos = link.indexOf('#');
 				var anchorcontent = '';
@@ -63,78 +129,14 @@
 					anchorcontent = link.substring(anchor_pos+1);
 					link = link.substring(0, anchor_pos);
 				}
-
+	
 				link += (link.indexOf('?') > -1 ? '&' : '?') + query_string;
 				if(anchorcontent.length) {
 					link += '#' + anchorcontent;
 				}
-
-				links[i].setAttribute('href', link);
-			}
-		}
-
-		// add to forms
-		var forms = document.querySelectorAll(form_selector);
-		for (var i=0;i<forms.length;i++) {
-			var action = forms[i].getAttribute('action');
-			var method = forms[i].getAttribute('method');
-
-			// if post, add to action, otherwise add hidden inputs.
-			if (method && method.toLowerCase() == 'post') {
-				action = action === null ? '' : action; ;
-				if (action.charAt(0) != '#') {
-					var anchor_pos = action.indexOf('#');
-					var anchorcontent = '';
-					if (anchor_pos > -1) {
-						anchorcontent = action.substring(anchor_pos+1);
-						action = action.substring(0, anchor_pos);
-					}
-
-					action += (link.indexOf('?') > -1 ? '&' : '?') + query_string;
-					if (anchorcontent.length) {
-						action += '#' + anchorcontent;
-					}
-
-					forms[i].setAttribute('action', action);
-				}
-			} else {
-				for (var j=0;j<found_params.length;j++) {
-					var p = found_params[j];
-					var input = document.createElement('input');
-					input.setAttribute('name', p.name);
-					input.setAttribute('value', p.value);
-					input.setAttribute('type', 'hidden');
-					forms[i].appendChild(input);
-				}
-			}
-		}
-	} else {
-		var query_string = '';
-	}
 	
-	// add to iframes
-	var iframes = document.querySelectorAll(iframe_selector);
-	for(var i=0;i<iframes.length;i++) {
-		var link = iframes[i].getAttribute('src');
-		if (!link || link == undefined || link.length == 0) {
-			link = iframes[i].getAttribute('data-original-src');
-		}
-		if(link.length && link.charAt(0) != '#') {
-			var anchor_pos = link.indexOf('#');
-			var anchorcontent = '';
-			if(anchor_pos > -1) {
-				anchorcontent = link.substring(anchor_pos+1);
-				link = link.substring(0, anchor_pos);
+				iframes[i].setAttribute('src', link);
 			}
-
-			link += (link.indexOf('?') > -1 ? '&' : '?') + query_string;
-			if(anchorcontent.length) {
-				link += '#' + anchorcontent;
-			}
-
-			iframes[i].setAttribute('src', link);
 		}
-	}
-
-
+	});
 }).call(this);
